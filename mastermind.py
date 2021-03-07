34c6621b95f7c842580e37f9a0d0a6414c35f60c
Nathanb-hub <nath.buchin@gmail.com>
2021-03-07 12:53:45 +0100

    Function colors info

--- mastermind.py
+++ mastermind.py
@@ -8,12 +8,11 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 import random
 
-
 red = [1, 0, 0, 1]  
 green = [0, 1, 0, 1]  
 blue = [0, 0, 1, 1]  
 purple = [1, 0, 1, 1]  
-yelllow = []
+yellow = [1,1,0,1]
 white = [1,1,1,1]
 black = [0,0,0,0]
 
@@ -24,14 +23,46 @@ thirdDot = Spinner(text='Choose color', values=colors,size_hint=(1, 1))
 lastDot = Spinner(text='Choose color', values=colors,size_hint=(1, 1))
 
 class Main(App):
-    def checkColors(self,instance):
-        c1 = firstDot.text
-        if (self.winningCombination[0] == c1):
-            print("ok")
+    def getInfos(self,instance):
+        inputs = [firstDot.text,secondDot.text,thirdDot.text,lastDot.text]
+        for i in range(len(inputs)):
+            # section to give informations to player
+            if (inputs[i]  == self.winningCombination[i]): # right color at the right place
+                self.infoButtons[i].background_color=[0,1,0,1]
+            elif(inputs[i]  in self.winningCombination and self.winningCombination[i] != inputs[i]):#right color at the wrong place
+                self.infoButtons[i].background_color= yellow 
+            elif(inputs[i] not in self.winningCombination): #wrong color
+                self.infoButtons[i].background_color= [1,0,0,1]
+        self.adjustGridColors()
+            # if current_color=='Red':
+            #     current_color = [1,0,0,1]
+            # elif current_color=='Blue':
+            #     current_color = [0,0,1,1]
+            # elif current_color=='Green':
+            #     current_color = [0,1,0,1]
+            # elif current_color=='White':
+            #     current_color = [1,1,1,1]
+            # elif current_color=='Black':
+            #     current_color = [0,0,0,0]
+            # else :
+            #     pass
+    def adjustGridColors(self):
+        inputs = [firstDot.text,secondDot.text,thirdDot.text,lastDot.text]
+        chunks = (inputs,4)
+        print(chunks)
+        
+    
+        for i in range(len(self.gridButtons)):
+            if (round ==1):
+                pass#self.gridButtons[i].background_color = 
+            
+        
+        #section to adjust the color of the grid        
 
     def build(self):
         self.title= 'Mastermind'
-        
+        self.infoButtons = []
+        self.gridButtons = []
         # les couleurs a obtenir pour gagner 
         self.winningCombination =[random.choice(colors),random.choice(colors),random.choice(colors),random.choice(colors)] 
         print(self.winningCombination)
@@ -63,7 +94,8 @@ class Main(App):
         # grille de jeu 
         gameGrid = GridLayout(cols=4,rows=11,size_hint=(0.7,1))
         for i in range(1,41):
-            btn = Button(text='')   
+            btn = Button(text='')
+            self.gridButtons.append(btn)   
             gameGrid.add_widget(btn)
         gameGrid.add_widget(firstDot)
         gameGrid.add_widget(secondDot)
@@ -73,6 +105,7 @@ class Main(App):
         rightMargin = BoxLayout(orientation='vertical',size_hint=(0.1,1))
         r_label = Label(text="",size_hint=(1,0.9))
         but1 = Button(text='Submit',background_color=[0, 1, 0, 1],size_hint=(1,0.1))
+        but1.bind(on_press=self.getInfos)
         rightMargin.add_widget(r_label)
         rightMargin.add_widget(but1)
 
@@ -86,7 +119,8 @@ class Main(App):
         lab1 = Label(text="",size_hint=(1,0.2))
         infoGrid = GridLayout(cols=4,rows=1,size_hint=(1,0.6))
         for i in range(0,4):
-            btn = Button(text='')   
+            btn = Button(text='') 
+            self.infoButtons.append(btn)  
             infoGrid.add_widget(btn)
         lab2 = Label(text="",size_hint=(1,0.2))
         footer_content.add_widget(lab1)
