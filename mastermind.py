6bddd6a781b039ee108db96f2236f67c4e9823d4
Nathanb-hub <nath.buchin@gmail.com>
2021-03-07 01:15:08 +0100

    UI changes

--- mastermind.py
+++ mastermind.py
@@ -9,6 +9,14 @@ from kivy.uix.gridlayout import GridLayout
 import random
 
 
+red = [1, 0, 0, 1]  
+green = [0, 1, 0, 1]  
+blue = [0, 0, 1, 1]  
+purple = [1, 0, 1, 1]  
+yelllow = []
+white = [1,1,1,1]
+black = [0,0,0,0]
+
 colors = ['Red','Blue','Yellow','Green','White','Black']
 firstDot = Spinner(text='Choose color', values=colors,size_hint=(1, 1))
 secondDot = Spinner(text='Choose color', values=colors,size_hint=(1, 1))
@@ -16,51 +24,86 @@ thirdDot = Spinner(text='Choose color', values=colors,size_hint=(1, 1))
 lastDot = Spinner(text='Choose color', values=colors,size_hint=(1, 1))
 
 class Main(App):
+    def checkColors(self,instance):
+        c1 = firstDot.text
+        if (self.winningCombination[0] == c1):
+            print("ok")
+
     def build(self):
         self.title= 'Mastermind'
+        
         # les couleurs a obtenir pour gagner 
-        firstColor = random.choice(colors)
-        secondColor = random.choice(colors)
-        thirdColor = random.choice(colors)
-        lastColor = random.choice(colors)
+        self.winningCombination =[random.choice(colors),random.choice(colors),random.choice(colors),random.choice(colors)] 
+        print(self.winningCombination)
 
 
         # pour contenir toutes les sections
-        gamePage = BoxLayout(orientation='horizontal')
-        #section pour donner les indices : bonne couleur au bon endroit ou juste bone couleur 
-        commentSection = BoxLayout(orientation='vertical',size_hint=(0.3,1))
-        indi = Label(text='Indices :',size_hint=(1,.4))
-        firstInd = Label(text='Ici le premier indice',size_hint=(1,.2))
-        seconInd = Label(text='Ici le deuxieme indice',size_hint=(1,.2))
-        valid_btn = Button(text='Valider',size_hint=(1,.2))
-        #valid.bind(on_press=checkColors)
-        commentSection.add_widget(indi)
-        commentSection.add_widget(firstInd)
-        commentSection.add_widget(seconInd)
-        commentSection.add_widget(valid_btn)
+        gamePage = BoxLayout(orientation='vertical')
+        #section pour le titre 
+        header  = BoxLayout(orientation ="horizontal",size_hint=(1,0.15))
+        b1 = Button(text='Menu',background_color=[1, 0, 1, 1],size_hint=(0.1,1))
+        header_label = Label(text="Mastermind",font_size="25sp",size_hint=(0.8,1))
+        b2 = Button(text='Scores',background_color=[1, 0, 1, 1],size_hint=(0.1,1))
+        header.add_widget(b1)
+        header.add_widget(header_label)
+        header.add_widget(b2)
+        
+
+
+
+
+        #Section pour le jeu : decomposée en 3 parties 
+        gameSection = BoxLayout(orientation='horizontal',size_hint=(1,0.75))
+
+        leftMargin = BoxLayout(orientation='vertical',size_hint=(0.1,1))
+        l_label = Label(text="",size_hint=(1,0.9))
+        leftMargin.add_widget(l_label)
         
 
         # grille de jeu 
-        gameSection = GridLayout(cols=4,rows=11,size_hint=(0.7,1))
+        gameGrid = GridLayout(cols=4,rows=11,size_hint=(0.7,1))
         for i in range(1,41):
-            btn = Button(text='',id=str(i))
-            gameSection.add_widget(btn)
-        gameSection.add_widget(firstDot)
-        gameSection.add_widget(secondDot)
-        gameSection.add_widget(thirdDot)
-        gameSection.add_widget(lastDot)
+            btn = Button(text='')   
+            gameGrid.add_widget(btn)
+        gameGrid.add_widget(firstDot)
+        gameGrid.add_widget(secondDot)
+        gameGrid.add_widget(thirdDot)
+        gameGrid.add_widget(lastDot)
+
+        rightMargin = BoxLayout(orientation='vertical',size_hint=(0.1,1))
+        r_label = Label(text="",size_hint=(1,0.9))
+        but1 = Button(text='Submit',background_color=[0, 1, 0, 1],size_hint=(1,0.1))
+        rightMargin.add_widget(r_label)
+        rightMargin.add_widget(but1)
+
+        gameSection.add_widget(leftMargin)
+        gameSection.add_widget(gameGrid)
+        gameSection.add_widget(rightMargin)
+
+        # pied de page 
+        footer = BoxLayout(orientation ="horizontal",size_hint=(1,0.10))
+        footer_content = BoxLayout(orientation="vertical")
+        lab1 = Label(text="",size_hint=(1,0.2))
+        infoGrid = GridLayout(cols=4,rows=1,size_hint=(1,0.6))
+        for i in range(0,4):
+            btn = Button(text='')   
+            infoGrid.add_widget(btn)
+        lab2 = Label(text="",size_hint=(1,0.2))
+        footer_content.add_widget(lab1)
+        footer_content.add_widget(infoGrid)
+        footer_content.add_widget(lab2)
+        footer.add_widget(footer_content)
+
+    
 
 
-
-        gamePage.add_widget(commentSection)
+        gamePage.add_widget(header)
         gamePage.add_widget(gameSection)
+        gamePage.add_widget(footer)
         return gamePage
         # on ajoute toutes les sections dans la page de jeu avant de la return
 
 
-        def checkColors(self):
-            
-            
-            firstDot.text
+
 
 Main().run()
